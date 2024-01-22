f0a7d6a1518f6eae37f9bb04bd778bb4d4f5598d
trey-rosius <treyrosius@gmail.com>
2024-01-22 14:52:43 +0100

    more updates

--- src/bin/lambda/process-cart-products.rs
+++ src/bin/lambda/process-cart-products.rs
@@ -7,9 +7,9 @@ use aws_lambda_events::dynamodb::Event;
 use rayon::prelude::*;
 use lambda_runtime::{LambdaEvent,service_fn, Error};
 use aws_config::{BehaviorVersion,load_defaults};
-use aws_sdk_sqs::{Client as SqsClient, types::SendMessageBatchRequestEntry};
+use aws_sdk_sqs::Client as SqsClient;
 use tracing::info;
-use ecommerce_api::model:: Order;
+
 
 
 type E = Box<dyn std::error::Error + Sync + Send + 'static>;
@@ -53,7 +53,7 @@ async fn process_dynamodb_streams(
        
                   
                         info!("new image record {:?}",&record.clone().change.new_image);
-                        info!("new image String is {}",new_image);
+                      
 
                        
 
@@ -61,8 +61,7 @@ async fn process_dynamodb_streams(
     }).collect();
     info!("all images String is {:?}",events);
     for item in events{ 
-          let order: Order = serde_json::from_str(&item).unwrap();
-          info!("structured order is {:?}",order);
+         
          sqs_client.send_message()
                         .queue_url(sqs_queue_url)
                         .message_body(item)
